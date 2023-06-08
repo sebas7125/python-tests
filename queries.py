9fc7780b024a6a663a9ae791549888c0c93d1fb3
sebas7125snow <sebas7125snowflake@gmail.com>
2023-06-08 14:14:06 -0500

    Adding filtering

--- queries.py
+++ queries.py
@@ -3,5 +3,6 @@ def count(table_name):
     return sql
 
 def join(table_a, table_b):
-    sql = f'select count(*) from {table_a} a join {table_b} b on a.l_orderkey = b.l_orderkey'
+    sql = f'''select count(*) from {table_a} a join {table_b} b on a.l_orderkey = b.l_orderkey
+        where a.l_shipdate between '1993-01-01' and '1996-12-31\''''
     return sql
