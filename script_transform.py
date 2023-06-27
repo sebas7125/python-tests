5648994b421a2006eae66f5cb095c5311a7922e3
sebas7125snow <sebas7125snowflake@gmail.com>
2023-06-27 15:04:40 -0500

    Script finished

--- script_transform.py
+++ script_transform.py
@@ -1,17 +1,16 @@
 import os
-import re
-import subprocess
-import requests
-from requests_negotiate_sspi import HttpNegotiateAuth
 
-mirror_url = r'C:\Users\sebas\Documents\GitHub\Blankfactor\ethos-snowflake-mirror'
-delta_dir = 'SDM\RDS\Delta'
-history_dir = 'SDM\RDS\History'
-script_name = 'PARTICIPANT_ACCOUNT_ROLLOVER_DISBURSEMENT'
+#/Users/sebastian.martinez/Documents/Git/ethos-snowflake-mirror/SDM/RDS/Delta/PARTICIPANT_ACCOUNT_ROLLOVER_DISBURSEMENT_delta.sql
+#/Users/sebastian.martinez/Documents/Git/python-tests/script_transform.py
 
-delta_path = os.path.join(mirror_url, delta_dir, script_name + '_delta.sql')
-history_path = os.path.join(mirror_url, history_dir, script_name + '_hist.sql')
+mirror_project = '../ethos-snowflake-mirror'
+delta_dir = 'SDM/RDS/Delta'
+history_dir = 'SDM/RDS/History'
+script_name = 'PARTICIPANT_ACCOUNT_AUTO_ENROLLMENT_ELECTION'
+automation_project = script_name + '.sql'
 
+delta_path = os.path.join(mirror_project, delta_dir, script_name + '_delta.sql')
+history_path = os.path.join(mirror_project, history_dir, script_name + '_hist.sql')
 
 # Return the minimum index of a list different than -1, if there is
 def get_min_index(indexes):
@@ -73,31 +72,78 @@ def replace_tables(script):
     replace_tables(script)
   else:
     return
+  
+# Add Jinja tags: delta and history flags for dynamic content
+def add_jinja_tags():
+  global query
+  # Get query lines with no blank lines
+  query_lines = [line for line in query.splitlines() if line.strip()]
+  range = len(query_lines)
+  index = 0
+  while index < range:
+    if 'UNION ALL' in query_lines[index].upper():
+      if '{{source_context}}' in query_lines[index-1] and '{{source_hist_context}}' in query_lines[index+1]:
+        if query_lines[index+1][-1] == ')':
+          query_lines[index+1] = query_lines[index+1][:-1]
+          query_lines.insert(index+2, '{% endif %}\n  )')
+        else:
+          query_lines.insert(index+2, '{% endif %}')
+        query_lines.insert(index, '{% if delta %}')
+        index += 2
+        range += 2
+    elif 'MERGE_DELTA' in query_lines[index].upper():
+      query_lines.insert(index+1, '{% endif %}')
+      query_lines.insert(index, '{% if delta %}')
+      index += 2
+      range += 2
+      if 'MERGE_HIST' in history_script.upper():
+        line = next(line for line in history_script.splitlines() if 'MERGE_HIST' in line.upper())
+        query_lines.insert(index+1, '{% endif %}')
+        query_lines.insert(index+1, line.upper().replace('CUR_ETHOS_RETIREMENT.SDM', '{{target_context}}'))
+        query_lines.insert(index+1, '{% if history %}')
+        index += 3
+        range += 3
+    elif 'ARRIVALTIMESTAMP >' in query_lines[index].upper():
+      if 'WHERE' in query_lines[index].upper():
+        query_lines[index] = query_lines[index].upper().replace('WHERE ', 'WHERE\n{% if delta %}\n')
+        query_lines.insert(index+2, '{% endif %}')
+        index += 1
+        range += 1
+      else:
+        query_lines.insert(index, '{% if delta %}')
+        query_lines.insert(index+3, '{% endif %}')
+        index += 2
+        range += 2
+      if '$HIST_CUT_OFF_DT' in history_script.upper():
+        line = next(line for line in history_script.splitlines() if '$HIST_CUT_OFF_DT' in line.upper())
+        query_lines.insert(index+2, '{% endif %}')
+        query_lines.insert(index+2, line.upper().replace('WHERE', 'AND'))
+        query_lines.insert(index+2, '{% if history %}')
+        index += 3
+        range += 3
+    index += 1
+  query = '\n'.join(query_lines)
 
-# Add Jinja tags: delta and history, for UNION ALL clauses of source_context and source_hist_context
-
-# Reading the files
+# Read the files
 with open(delta_path, 'r') as file:
   delta_script = file.read()
 with open(history_path, 'r') as file:
   history_script = file.read()
 
-# Getting the query
+# Get the query
 insert_index = find(delta_script, 'INSERT INTO IDENTIFIER', 0)
 query_start = get_min_index([find(delta_script, 'WITH', insert_index), find(delta_script, 'SELECT', insert_index)])
 query_end = find(delta_script, ';', find(delta_script, 'WHERE', query_start))
 query = delta_script[query_start:query_end]
 
-# Replacing table identifiers for the name and context of the tables
+# Replace table identifiers for the name and context of the tables
 replace_tables(delta_script)
 
+# Add jinja flags for dynamic content
+add_jinja_tags()
 
 print(query)
 
-# Perform modifications on the script content
-# modified_content = script_content.replace('<<variable_name>>', 'variable_value')
-# modified_content = re.sub(r'{%.*?%}', '', modified_content)  # Remove Jinja tags
-
 # Write the modified script to the automation repository
-# with open(os.path.join(automation_dir, filename), 'w') as file:
-#     file.write(modified_content)
+with open(automation_project, 'w') as file:
+  file.write(query)
